e7b0ab6059d0807f643ef4b9171f802f60cf72b9
Selvi Ece Dugan <selvidugan@gmail.com>
2022-10-27 21:03:24 +0300

    js on progress

--- madlibs.js
+++ madlibs.js
@@ -26,17 +26,65 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
+
 function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
+  const dot = "."
+  const comma = ","
+  const noun = "[n]"
+  const verb = "[v]"
+  const adjective = "[a]"
+  rawStory = rawStory.replace(dot, " .");
+  rawStory = rawStory.replace(comma, " , ");
+  const storyArray = rawStory.split(" ")
+  const objectArray = []
+  storyArray.forEach(word => {
+    const length = word.length - 3
+    if (word.includes(noun)) {
+      const object = {}
+      object["word"] = word.slice(0, length);
+      object["pos"] = "noun"
+      objectArray.push(object)
+    }
+    else if (word.includes(verb)) {
+      const object = {}
+      object["word"] = word.slice(0, length);
+      object["pos"] = "verb"
+      objectArray.push(object)
+    }
+    else if (word.includes(adjective)) {
+      const object = {}
+      object["word"] = word.slice(0, length);
+      object["pos"] = "adjective"
+      objectArray.push(object)
+    }
+    else {
+      const object = {}
+      object["word"] = word
+      objectArray.push(object)
+    }
+  });
+
+  return objectArray;
+}
+const createTextEdit = (arr) => {
+  const editDOM = document.querySelector(".madLibsEdit")
+  const paragraph = document.createElement("p")
+  const idCount = 0
+  arr.forEach(item => {
+    if (item.pos) {
+      const Input = document.createElement("input");
+      Input.setAttribute(maxlenght, 20)
+      Input.setAttribute(id, idCount++)
+    }
+  })
 }
 
 /**
  * All your other JavaScript code goes here, inside the function. Don't worry about
  * the `then` and `async` syntax for now.
- * 
+ *
  * You'll want to use the results of parseStory() to display the story on the page.
  */
 getRawStory().then(parseStory).then((processedStory) => {
   console.log(processedStory);
-});
+})
